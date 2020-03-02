04211855000e961687a70d09cefe41166564e3aa
jeongyong95 <jeongyong95@gmail.com>
2020-03-02 20:05:04 +0900

    백준 단계별 문자열까지 문제풀이

---
+++ Solution10809.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*  알파벳 찾기
+    알파벳 소문자로만 이루어진 단어 S가 주어진다.
+    각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 
+    포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+    첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
+    각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
+    만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다.
+    단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
+ */
+public class Solution10809 {
+    // 복습횟수 : 0 , 해답봤음
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(word.indexOf(i));
+        }
+        scanner.close();
+    }
+}
---
+++ test.java
@@ -0,0 +1,16 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        char la = 'A';
+        System.out.println((int) a);
+        System.out.println((int) la);
+
+        String string = "ljes=njak";
+        string = string.replace("s=", "");
+        System.out.println(string);
+    }
+}
