fbfaca6e25a09927219aa0d07f76a409b8df1fe1
jeongyong95 <jeongyong95@gmail.com>
2020-03-20 18:31:05 +0900

    문자열 문제풀기 시작

--- test.java
+++ test.java
@@ -1,12 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
- * test
+ * test & setting
  */
 public class test {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        bufferedReader.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 
     // BufferedReader 와 BufferedWriter를 사용하려면
