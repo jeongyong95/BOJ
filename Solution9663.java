f509473c02b001889d2cc4f44ecf9510f2cf1632
Jeongyong Joo <jeongyong95@gmail.com>
2020-05-02 22:52:14 +0900

    동적계획법 일부와 그리디 2문제

--- Solution9663.java
+++ Solution9663.java
@@ -11,16 +11,42 @@ import java.io.OutputStreamWriter;
     첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
  */
 public class Solution9663 {
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int count = 0;
+    static int[] board;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bufferedReader.readLine());
+        board = new int[n];
         bufferedReader.close();
 
+        backTracking(board, n, 0);
+        bufferedWriter.write(count + "\n");
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 
-    static void backTracking() throws IOException {
+    static void backTracking(int[] board, int n, int index) {
+        if (index == n) {
+            count++;
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            board[index] = i;
+            if (isPossibleLocation(board, index)) {
+                backTracking(board, n, index + 1);
+            }
+        }
         return;
+    }
+
+    static boolean isPossibleLocation(int[] board, int index) {
+        for (int i = 0; i < index; i++) {
+            if (board[index] == board[i] || index - i == Math.abs(board[index] - board[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- test.java
+++ test.java
@@ -1,31 +1,16 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 /**
  * test & setting
  */
 public class test {
+    public static void main(String[] args) {
+        int[] num = new int[100];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = i * 3;
+        }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        bufferedReader.close();
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        int sum = Arrays.stream(num).filter(n -> (n % 2 == 1)).sum();
+        System.out.println(sum);
     }
-
-    // BufferedReader 와 BufferedWriter를 사용하려면
-
-    // 1. import java.io.*; 선언
-    // 2. throws IOException 으로 사용하는 메소드에 예외처리를 위임해라 ( try ~ catch로 해도 되지만.. )
-    // 3. readLine() 은 String만 리턴한다. 다른 자료형으로 활용하려면 형변환해라.
-    // 4. reader도 writer도 사용후엔 스트림을 닫아주자.
-    // writer는 닫기전에 flush를 활용해 남아있는 데이터를 모두 출력시켜야한다.
-    // 5. 개행처리는 +"\n"을 활용하는 걸 추천한다. newLine() 을 활용할 수도 있지만 개행문자와 비교하면 속도가 느리고
-    // 다른 자료형을 출력할 땐 String으로 형변환도 해주니
-    // int를 출력했는데 아스키 코드 숫자에 해당하는 문자가 출력되는 일을 막아줄 수도 있다.
 }
