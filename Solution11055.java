b1c30d7040a52d7bfe25e01c0ce6320eb96214c0
Joo Jeongyong <jeongyong95@gmail.com>
2021-02-16 17:12:23 +0900

    dp 가장 긴 감소하는 부분수열까지

---
+++ Solution11055.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Solution11055 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        br.close();
+
+        int[] dp = Arrays.copyOf(arr, arr.length);
+
+        for (int i = 1; i < a; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+                }
+            }
+        }
+
+        int max = dp[0];
+        for (int i : dp) {
+            if (max <= i) {
+                max = i;
+            }
+        }
+
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ Solution11722.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Solution11722 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        br.close();
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for (int i : dp) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
