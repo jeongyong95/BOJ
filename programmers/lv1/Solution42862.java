d7563515b9ce6c5e94d15ba7f8216f0835b38290
Joo Jeongyong <jeongyong95@gmail.com>
2021-03-15 23:35:12 +0900

    프로그래머스 그리디 알고리즘 lv1 체육복 풀이

--- programmers/lv1/Solution42862.java
+++ programmers/lv1/Solution42862.java
@@ -1,22 +1,32 @@
 package programmers.lv1;
 
-import java.util.Arrays;
-
 public class Solution42862 {
-    // 틀렸당
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        answer = n - lost.length;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        int[] nArr = new int[n];
 
-        for (int i = lost.length - 1; i >= 0; i--) {
-            for (int j = reserve.length - 1; j >= 0; j--) {
-                if (reserve[j] == lost[i] + 1 || reserve[j] == lost[i] - 1) {
-                    reserve[j] = -1;
-                    lost[i] = -1;
-                    answer++;
+        for (int i : lost) {
+            nArr[i - 1]--;
+        }
+
+        for (int i : reserve) {
+            nArr[i - 1]++;
+        }
+
+        for (int i = 0; i < nArr.length; i++) {
+            if (nArr[i] < 0) {
+                if (i != 0 && nArr[i - 1] > 0) {
+                    nArr[i]++;
+                    nArr[i - 1]--;
+                } else if (i != n - 1 && nArr[i + 1] > 0) {
+                    nArr[i]++;
+                    nArr[i + 1]--;
                 }
+            }
+        }
+        for (int i : nArr) {
+            if (i >= 0) {
+                answer++;
             }
         }
         return answer;
---
+++ programmers/lv2/Solution42860.java
@@ -0,0 +1,8 @@
+package programmers.lv2;
+
+public class Solution42860 {
+    public int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
