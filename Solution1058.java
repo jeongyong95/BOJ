b90a0670d19b3758470655bae478f711a3eb2d56
Joo Jeongyong <jeongyong95@gmail.com>
2021-04-05 19:30:38 +0900

    백준 실버 dfs 침투까지

---
+++ Solution1058.java
@@ -0,0 +1,5 @@
+public class Solution1058 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Solution14502.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution14502 {
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        for (int i = 0; i < map.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        br.close();
+
+        bw.write(bfs() + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    static int bfs() {
+        int answer = 0;
+        return answer;
+    }
+}
