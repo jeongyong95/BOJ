d94acb5bd12aacaaa956bc06ad3245ad40e3e8c8
Joo Jeongyong <jeongyong95@gmail.com>
2021-06-10 22:53:16 +0900

    프로그래머스 고득점킷 그래프 탐색 풀이 완료

--- programmers/lv3/Solution43163.java
+++ programmers/lv3/Solution43163.java
@@ -1,10 +1,54 @@
 package programmers.lv3;
 
-// TODO : 프로그래머스 킷 그래프탐색 풀어야 함
-public class Solution43163 {
-    public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
+public class Solution43163 {
+    static Queue<String> q = new LinkedList<>();
+    static Map<String, Boolean> visited = new HashMap<>();
+    static Map<String, Integer> dist = new HashMap<>();
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = bfs(begin, target, words);
         return answer;
+    }
+
+    static int bfs(String begin, String target, String[] words) {
+        q.offer(begin);
+        visited.put(begin, true);
+        dist.put(begin, 0);
+
+        while (!q.isEmpty()) {
+            String current = q.poll();
+
+            for (String s : words) {
+                if (isPossibleNext(current, s)) {
+                    if (!visited.getOrDefault(s, false)) {
+                        if (s.equals(target)) {
+                            return dist.get(current) + 1;
+                        }
+                        visited.put(s, true);
+                        dist.put(s, dist.get(current) + 1);
+                        q.offer(s);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    static boolean isPossibleNext(String current, String target) {
+        int diff = 0;
+        for (int i = 0, size = current.length(); i < size; i++) {
+            if (current.charAt(i) != target.charAt(i)) {
+                diff++;
+            }
+        }
+        if (diff > 1) {
+            return false;
+        }
+        return true;
     }
 }
---
+++ programmers/lv3/Solution43164.java
@@ -0,0 +1,76 @@
+package programmers.lv3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution43164 {
+    static Map<String, List<Ticket>> ticketMap = new HashMap<>();
+    static Map<String, Boolean> visited = new HashMap<>();
+    static String[] answer;
+    static boolean isSolved = false;
+
+    public static void main(String[] args) {
+        String[][] tickets = { { "ICN", "COO" }, { "ICN", "BOO" }, { "COO", "ICN" }, { "BOO", "DOO" } };
+        solution(tickets);
+    }
+
+    public static String[] solution(String[][] tickets) {
+
+        for (String[] s : tickets) {
+            List<Ticket> nextList = ticketMap.getOrDefault(s[0], new ArrayList<>());
+            nextList.add(new Ticket(s[1], false));
+            ticketMap.put(s[0], nextList);
+        }
+
+        for (String key : ticketMap.keySet()) {
+            List<Ticket> nextList = ticketMap.get(key);
+            Collections.sort(nextList);
+        }
+        answer = new String[tickets.length + 1];
+        answer[0] = "ICN";
+        dfs(answer[0], 1);
+        return answer;
+    }
+
+    static void dfs(String current, int index) {
+        if (index == answer.length) {
+            isSolved = true;
+            return;
+        }
+
+        List<Ticket> nextList = ticketMap.get(current);
+        if (nextList == null) {
+            return;
+        }
+
+        for (Ticket ticket : nextList) {
+            if (!ticket.isUsed) {
+                ticket.isUsed = true;
+                answer[index] = ticket.next;
+                dfs(ticket.next, index + 1);
+                if (isSolved) {
+                    return;
+                }
+                ticket.isUsed = false;
+            }
+        }
+    }
+}
+
+class Ticket implements Comparable<Ticket> {
+    String next;
+    boolean isUsed;
+
+    public Ticket(String next, boolean isUsed) {
+        this.next = next;
+        this.isUsed = isUsed;
+    }
+
+    @Override
+    public int compareTo(Ticket o) {
+        return this.next.compareTo(o.next);
+    }
+}
